94daad73c7d8dbea4c69e8088ef82c95876e5eb0
Alexandria Hart <alexandriahart@moondog.local>
2018-11-18 17:31:30 -0700

    added intital landing page routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import {Switch, Route, withRouter } from 'react-router-dom'
 import './App.css';
+
+import Landing from './components/Landing/Landing'
 
 class App extends Component {
   render() {
     return (
-      <div>
-        This is App.js for Herbarium project
+      <div className='App'>
+        <Switch>
+          <Route component={Landing} exact path='/'/>
+        </Switch>
       </div>
     );
   }
 }
-
-export default App;
+export default withRouter(App)
